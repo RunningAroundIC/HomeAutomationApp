cd86a471a9afeffad62a347b2aafa92c69bdb81f
Karsten <karsten@raic.dk>
2018-01-26 09:23:46 +0100

    Exam edition.
    This is the latest version.

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from "@angular/http";
 import { Observable } from 'rxjs';
-import { Settings } from './Settings';
+import { Settings } from './settings';
 
 import 'rxjs/add/operator/map';
 
